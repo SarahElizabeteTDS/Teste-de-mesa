e5328ca6cb7582d043de7e69ec7da560929f9c56
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2023-11-20 01:04:25 -0300

    Update exercicio-8.php

--- exercicio-8.php
+++ exercicio-8.php
@@ -14,3 +14,16 @@
     print "\nTeste 5: ";
       var_dump(shuffle($lista_frutas));
 
+/*
+  Teste 0: int(4)     --->  quantidade de elementos no array
+
+  Teste 1: bool(true)  ---> quer dizer que a string "banana" está no array
+  
+  Teste 2: bool(false)  ---> quer dizer que a string "alface" não está no array
+  
+  Teste 3: int(3)     ---> 3 é a posicao do "morango", no array, pois, o array comeca em 0
+  
+  Teste 4: bool(true)  ---> a funcao sort retorna o valor falso quando o sort é mal-sucedido, como ele reordenou corretamente, retornou verdadeiro.
+  
+  Teste 5: bool(true)  ---> mesma coisa que a funcao sort.
+*/
