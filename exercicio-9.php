c78888aa0171d89c5156c98195c53d524a093530
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2023-11-20 01:14:22 -0300

    Update exercicio-9.php

--- exercicio-9.php
+++ exercicio-9.php
@@ -20,19 +20,19 @@
         var_dump(sort($pessoa));
 
 /*
-Teste 0: int(5)
+Teste 0: int(5) ---> O teste 0, conta quantos elementos tem o array.
 
-Teste 1: bool(true)
+Teste 1: bool(true) ---> Verifica se a string "fisico" está no array e ve que está.
 
-Teste 2: bool(false)
+Teste 2: bool(false) ---> Verifica se a string "detetive" está no array, ve que nao esta.
 
-Teste 3: bool(false)
+Teste 3: bool(false) ---> verifica se acha a string "nome", porém, "nome" é uma key.
 
-Teste 4: bool(true)
+Teste 4: bool(true) ---> verifica se acha a key "profissao", e achou!
 
-Teste 5: string(4) "nome"     
+Teste 5: string(4) "nome"  ---> mostra a primeira key, a "nome", que é uma string de 4 caracteres.
 
-Teste 6: string(9) "profissao"
+Teste 6: string(9) "profissao" ---> mostra a ultima key, a "profissao", que é uma string de 9 caracteres.
 
-Teste 7: bool(true)
+Teste 7: bool(true) ---> sort foi bem-sucedido!
 */
