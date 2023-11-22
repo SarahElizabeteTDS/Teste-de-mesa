1d5cec62cbf03f6e5e0f59255e77e450eed11c1b
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2023-11-21 21:54:31 -0300

    Update exercicio-10.php

--- exercicio-10.php
+++ exercicio-10.php
@@ -9,7 +9,18 @@
         $caractere = $apareceu;
     }
 
+    $teste = "Jeffinho é um querido";
+    $teste2 = 'e';
+
+    print $teste . "\n";
+
+    modoficarPorReferencia($teste, $teste2);
+
+    print $teste . "\n";
+
+    //veja que o é, não é substituido, pois na tabela ASCII, "e" e "é" são caracteres diferentes
+
 
     //https://www.php.net/manual/en/function.substr-count.php
     //https://www.php.net/manual/pt_BR/function.str-replace.php
-    //referencias de funcao, achei uns artigos mais antigos na net ai!
+    //referencias de funcao!!!
